ae9b5cb150508a12ee8dc4dbdc5cb18aafef418a
chetnap19 <67640344+chetnap19@users.noreply.github.com>
2020-07-18 18:12:10 +0530

    Switch Case Problem
    Switch Case Problem

---
+++ Day05/SwitchWeekday.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+read -p "Enter number between 0-6:" number
+case $number in
+	0)
+		echo "Sunday"
+		;;
+	1)
+		echo "Monday"
+		;;
+	2)
+		echo "Tuesday"
+		;;
+	3)
+		echo "Wednesday"
+		;;
+	4)
+		echo "Thursday"
+		;;
+	5)
+		echo "Friday"
+		;;
+	6)
+		echo "Saturday"
+		;;
+	*)
+		echo "Invalid Number"
+		;;
+	esac
+
